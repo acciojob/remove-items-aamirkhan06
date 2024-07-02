7694749f3300ec6357e86336aee1467361e5a064
aamirkhan06 <114560386+aamirkhan06@users.noreply.github.com>
2024-07-02 20:08:53 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,10 +1,4 @@
 //your JS code here. If required.
-let colors=document.getElementById("colorSelect");
-colors.innerHtml=`<option>Red</option> 
-				<option>Green</option> 
-				<option>White</option> 
-				<option>Black</option>`
-document.append(colors);
 function removeCol(){
 	let x = document.getElementById("colorSelect");
 	x.remove(x.selectedIndex)
